d726126020a723285ac4723254765245c72b632f
Rama Indrawan <rama.indrawan@yahoo.com>
2018-04-06 20:08:15 +0800

    Membuat Atribut dan Behavior

---
+++ src/praktek01/Jaket.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktek01;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Jaket {
+    String merek;
+    String ukuran;
+    String warna;
+    String bahan;
+    double harga;
+    
+void cetakInfo(){
+    System.out.println("Merek  :"+merek);
+    System.out.println("Ukuran :"+ukuran);
+    System.out.println("Warna  :"+warna);
+    System.out.println("Bahan  :"+bahan);
+    System.out.println("Harga  :"+harga);
+}    
+}
